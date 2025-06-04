c30939b87e386184a1938fbe12a149867b822df0
kruminskr <kruminskr@gmail.com>
2025-06-04 20:45:44 +0300

    style: format code

--- src/versions/v1/routes/index.js
+++ src/versions/v1/routes/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 const queryController = require('../services/controllers/query.controller');
 
-router.post('/data', queryController.queyProcesor);
+router.post('/data', queryController.processQuery);
 
 module.exports = router;
