86dec5e4c2a5faf4756a52d635ad5575989afc10
kruminskr <kruminskr@gmail.com>
2025-06-04 20:44:39 +0300

    docs: add AI model functionality comments

---
+++ src/versions/v1/services/models/ai.model.js
@@ -0,0 +1,23 @@
+// AI model
+// turns the data into a response
+// "Latvia's housing prices increased by 15%..."
+
+// function takes the data and a prompt and send it to AI API
+
+// Data will be in this format:
+// ### Housing Price Index
+
+// | Year | Value  |
+// |------|--------|
+// | 2020 | 100.2  |
+// | 2021 | 102.5  |
+// | 2022 | 105.7  |
+
+
+
+
+
+// TO-DO
+// 1. Returns just the text
+
+// 2. Returns the text and some charts
