4e55ce0911d26cee677a22c7a8ac8cc6c5947cfc
kruminskr <kruminskr@gmail.com>
2025-06-04 20:43:10 +0300

    build: enable JSON body parsing

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const v1Routes = require('./versions/v1/routes/index.js');
 
 app.use('/api/v1', v1Routes);
