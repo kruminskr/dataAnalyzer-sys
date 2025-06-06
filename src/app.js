e629035066250440df5e4c8b6f796987128095f2
kruminskr <kruminskr@gmail.com>
2025-06-06 15:29:38 +0300

    doc: add morgan

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 
 const v1Routes = require('./versions/v1/routes/index.js');
