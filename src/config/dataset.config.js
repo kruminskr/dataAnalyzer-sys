890b54f16543cdbbc17a4c9b177deccb9841b2dc
kruminskr <kruminskr@gmail.com>
2025-06-30 23:38:37 +0300

    feat: add more datasets

--- src/config/dataset.config.js
+++ src/config/dataset.config.js
@@ -47,10 +47,128 @@ const datasets ={
             age: 'TOTAL',
             sex: 'T',
         }
+    },
 
-    }
-}
+    // GDP
+    NAMA_10_GDP: {
+        id: 'NAMA_10_GDP',
+        staticParams: {
+            unit: 'CP_MEUR',
+            na_item: 'B1GQ',
+        },
+    },
+
+    // Employment rate of working-age population.
+    LFSA_ERGAN: {
+        id: 'LFSA_ERGAN',
+        staticParams: {
+            age: 'Y15-74',
+            sex: 'T',
+            citizen: 'TOTAL'
+        }
+    },
+
+    // Overall unemployment trends
+    UNE_RT_A: {
+        id: 'UNE_RT_A',
+        staticParams: {
+            age: 'Y15-74',
+            unit: 'PC_POP',
+            sex: 'T'
+        }
+    },
+
+    // Export value to all countries.
+    EXT_LT_INTRATRD: {
+        id: 'EXT_LT_INTRATRD',
+        staticParams: {
+            indic_et: 'MIO_EXP_VAL',
+            partner: 'WORLD',
+            sitc06: 'TOTAL',
+        }
+    },
+
+    // Education level ??
+    EDAT_LFSE_03: {
+        id: 'EDAT_LFSE_03',
+        staticParams: {
+            sex: 'T',
+            age: 'Y15-64',
+            isced11: 'ED5-8'
+        }
+    },
+
+    // Percentage of population leaving school early.
+    EDAT_LFSE_14: {
+        id: 'EDAT_LFSE_14',
+        staticParams: {
+            sex: 'T',
+            wstatus: 'POP'
+        }
+    },
+
+    // Share of renewable energy in gross consumption
+    NRG_IND_REN: {
+        id: 'NRG_IND_REN',
+        staticParams: {
+            nrg_bal: 'REN',
+        }
+    },
+
+    // General health outcomes and longevity
+    DEMO_MLEXPEC: {
+        id: 'DEMO_MLEXPEC',
+        staticParams: {
+            sex: 'T',
+            age: 'Y_LT1',
+        }
+    },
+
+    // Poverty and social inequality
+    ILC_PEPS01: {
+        id: 'ILC_PEPS01',
+        staticParams: {
+            unit: 'PC',
+            sex: 'T',
+            age: 'TOTAL',
+        }
+    },
+
+    // Income disparities across countries
+    ILC_DI03: {
+        id: 'ILC_DI03',
+        staticParams: {
+            unit: 'EUR',
+            sex: 'T',
+            age: 'TOTAL',
+            indic_il: 'MEI_E'
+        }
+    },
+
+
+    // Measures housing space adequacy
+    ILC_LVHO05A: {
+        id: 'ILC_LVHO05A',
+        staticParams: {
+            age: 'TOTAL',
+            sex: 'T',
+            incgrp: 'TOTAL',
+        }
+    },
+
+
+    // Economic competitiveness and workforce expenses
+    LC_LCI_R2_A: {
+        id: 'LC_LCI_R2_A',
+        staticParams: {
+            lcstruct: 'D11',
+            nace_r2: 'B-N',
+            unit: 'I16'
+        }
+    },
+};
 
 module.exports = {
     datasets
 }
+
