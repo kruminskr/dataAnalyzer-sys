eef0d654e8c35bc351e06ecd73466c29123ed20f
kruminskr <kruminskr@gmail.com>
2025-08-02 18:41:26 +0300

    feat: add cors for frontend intigration

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use(cors({
+  origin: process.env.APP_URL
+}));
 
 const v1Routes = require('./versions/v1/routes/index.js');
 
